1a649734dbce40b73bf171a8cf16c78778e81d26
Alexey <kret906alex@gmail.com>
2022-11-08 11:41:59 +0300

    SVG constants moved

--- src/main/java/visual/scheme/svg/ISVG.java
+++ src/main/java/visual/scheme/svg/ISVG.java
@@ -1,14 +1,5 @@
 package visual.scheme.svg;
 
 public interface ISVG {
-
-    String svgStart = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
-            "<svg version = \"1.1\"\n" +
-            "     baseProfile=\"full\"\n" +
-            "     xmlns = \"http://www.w3.org/2000/svg\" \n" +
-            "     height = \"400px\"  width = \"400px\">\n" +
-            "     <rect x=\"0\" y=\"0\" width=\"400\" height=\"400\" \n" +
-            "          fill=\"none\" stroke=\"black\" stroke-width=\"1px\" stroke-opacity=\"1\"/>\n";
-    String svgEnd = "</svg>";
     void flush(String fileName);
 }
--- src/main/java/visual/scheme/svg/SVG.java
+++ src/main/java/visual/scheme/svg/SVG.java
@@ -5,14 +5,23 @@ import java.io.IOException;
 
 public abstract class SVG implements ISVG {
 
-    protected String svgString = svgStart;
+    private static final String SVG_START = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+            "<svg version = \"1.1\"\n" +
+            "     baseProfile=\"full\"\n" +
+            "     xmlns = \"http://www.w3.org/2000/svg\" \n" +
+            "     height = \"400px\"  width = \"400px\">\n" +
+            "     <rect x=\"0\" y=\"0\" width=\"400\" height=\"400\" \n" +
+            "          fill=\"none\" stroke=\"black\" stroke-width=\"1px\" stroke-opacity=\"1\"/>\n";
+    private static final String SVG_END = "</svg>";
+
+    protected String svgString = SVG_START;
 
     protected final int radius = 10;
 
     public void flush(String filename) {
         try(FileWriter writer = new FileWriter(filename + ".svg", false)) {
             writer.write(svgString);
-            writer.write(svgEnd);
+            writer.write(SVG_END);
             writer.flush();
         } catch(IOException ex){
             System.out.println(ex.getMessage());
